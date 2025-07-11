1dbbac240aefb9950a3ee7b3d44d06973fb3e753
emilceinfante <emilceinfante@gmail.com>
2025-07-11 14:27:05 +0000

    app

--- src/app.py
+++ src/app.py
@@ -2,11 +2,12 @@ from flask import Flask, request, render_template
 from pickle import load
 
 app = Flask(__name__)
-model = load(open("/workspaces/app_Flask/models/random_forest_classifier_default_42.sav"))
+model = load(open("../models/random_forest_classifier_default_42.sav", "rb"))
 class_dict = {
     "0": "Introvertido",
     "1": "Extrovertido",
-    }
+
+}
 
 @app.route("/", methods = ["GET", "POST"])
 def index():
